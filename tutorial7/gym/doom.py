b0c8e0f530522d5d0e047ec3bd85e90b5c2b4396
Victor Mayoral Vilches <v.mayoralv@gmail.com>
2016-08-09 18:55:41 +0200

    tutorial7: doom silly example

--- tutorial7/gym/doom.py
+++ tutorial7/gym/doom.py
@@ -22,57 +22,26 @@ if __name__ == '__main__':
     number_of_features = env.observation_space.shape[0]
     last_time_steps = numpy.ndarray(0)
 
-    # Number of states is huge so in order to simplify the situation
-    # we discretize the space to: 10 ** number_of_features
-    cart_position_bins = pandas.cut([-2.4, 2.4], bins=n_bins, retbins=True)[1][1:-1]
-    pole_angle_bins = pandas.cut([-2, 2], bins=n_bins_angle, retbins=True)[1][1:-1]
-    cart_velocity_bins = pandas.cut([-1, 1], bins=n_bins, retbins=True)[1][1:-1]
-    angle_rate_bins = pandas.cut([-3.5, 3.5], bins=n_bins_angle, retbins=True)[1][1:-1]
+    action_attack = [False]*43
+    action_attack[0] = True
 
-    # The Q-learn algorithm
-    qlearn = QLearn(actions=range(env.action_space.n),
-                    alpha=0.5, gamma=0.90, epsilon=0.1)
+    action_right = [False]*43
+    action_right[10] = True
+    
+    action_left = [False]*43
+    action_left[11] = True
 
-    for i_episode in xrange(3000):
+    actions = [action_attack, action_left, action_right]
+
+    for i_episode in xrange(30):
         observation = env.reset()
-
-        cart_position, pole_angle, cart_velocity, angle_rate_of_change = observation            
-        state = build_state([to_bin(cart_position, cart_position_bins),
-                         to_bin(pole_angle, pole_angle_bins),
-                         to_bin(cart_velocity, cart_velocity_bins),
-                         to_bin(angle_rate_of_change, angle_rate_bins)])
-
         for t in xrange(max_number_of_steps):	    	
-            # env.render()
-
-            # Pick an action based on the current state
-            action = qlearn.chooseAction(state)
+            env.render()
             # Execute the action and get feedback
-            observation, reward, done, info = env.step(action)
+            observation, reward, done, info = env.step(env.action_space.sample())
+            if done:
+                break
 
-            # Digitize the observation to get a state
-            cart_position, pole_angle, cart_velocity, angle_rate_of_change = observation            
-            nextState = build_state([to_bin(cart_position, cart_position_bins),
-                             to_bin(pole_angle, pole_angle_bins),
-                             to_bin(cart_velocity, cart_velocity_bins),
-                             to_bin(angle_rate_of_change, angle_rate_bins)])
-
-            # # If out of bounds
-            # if (cart_position > 2.4 or cart_position < -2.4):
-            #     reward = -200
-            #     qlearn.learn(state, action, reward, nextState)
-            #     print("Out of bounds, reseting")
-            #     break
-
-            if not(done):
-                qlearn.learn(state, action, reward, nextState)
-                state = nextState
-            else:
-                # Q-learn stuff
-                reward = -200
-                qlearn.learn(state, action, reward, nextState)
-                last_time_steps = numpy.append(last_time_steps, [int(t + 1)])
-                break    
 
     l = last_time_steps.tolist()
     l.sort()
