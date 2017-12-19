ac56f5be1446bfe9362fa0dac93c1e0416ec2d2f
Victor Mayoral Vilches <v.mayoralv@gmail.com>
2017-12-19 09:41:04 +0100

    update in the code of tutorial 11

--- tutorial11/code/train_simplified_trader_dqn.py
+++ tutorial11/code/train_simplified_trader_dqn.py
@@ -12,16 +12,20 @@ def callback(lcl, glb):
 # env = gym.make("CartPole-v0")
 env = gym.make('simplified_trader-v0')
 model = deepq.models.mlp([64])
-act = deepq.learn(
-    env,
-    q_func=model,
-    lr=1e-3,
-    max_timesteps=100000,
-    buffer_size=50000,
-    exploration_fraction=0.1,
-    exploration_final_eps=0.02,
-    print_freq=10,
-    callback=callback
-)
+
+models = [[64], [64,64], [128,128], [256,256]]
+for m in models:
+    act = deepq.learn(
+        env,
+        q_func=deepq.models.mlp(m),
+        lr=1e-3,
+        max_timesteps=100000,
+        buffer_size=50000,
+        exploration_fraction=0.1,
+        exploration_final_eps=0.02,
+        print_freq=10,
+        callback=callback,
+        outdir="/tmp/experiments/DQN/"+str(m)
+    )
 print("Saving model to simplified_trader_dqn.pkl")
 act.save("simplified_trader_dqn.pkl")
